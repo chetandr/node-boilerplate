b06bdbb9b0388cc952e4d1eb6121cb2143e0573e
sonaliramkadam <sonali_kadam@persistent.com>
2020-01-07 16:56:03 +0530

    Added Object Builder services

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,20 +6,22 @@ import ProgramRouter from '../services/program/ProgramRoute';
 import TagRouter from '../services/program/TagRoute';
 import ProgramTypeRouter from '../services/program/ProgramTypeRoute';
 import LdapGroupRoute from '../services/program/LdapGroupRoute';
+import ObjectRouter from '../services/objectBuilder/ObjectRoute';
 
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/users',UserRouter);
-router.use('/workflows',UserRouter);
+router.use('/users', UserRouter);
+router.use('/workflows', UserRouter);
 router.use('/auth', AuthRouter);
 router.use('/params', ParamRouter);
-router.use('/programs',ProgramRouter);
+router.use('/programs', ProgramRouter);
 router.use('/programtypes', ProgramTypeRouter);
 router.use('/ldapgroups', LdapGroupRoute);
-router.use('/tags',TagRouter);
+router.use('/tags', TagRouter);
+router.use('/object', ObjectRouter);
 
 
 // Export the base-router
