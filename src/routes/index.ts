81b40f9f63dd19963fa102c2fbf22074a6097ac1
Poonam Rohani <poonam.rohani@seagate.com>
2020-05-13 17:14:58 +0530

    Added code to check server health status

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,6 +13,20 @@ router.use('/queryBuilder', QueryBuilderRouter);
 router.use('/workflowBuilder', WorkflowBuilderRouter);
 router.use('/prg', ProgramRouter);
 
+router.get('/no_auth/checkThis', function(req, res, next){
+    let myObj:any = {};
+    myObj.success = true;
+    myObj.message = "";
+    myObj.data = [];
+ 
+    var dateString = "Node.js server is up and running as of ";
+    var today = new Date();
+    dateString += today.toDateString() + " ";
+    dateString += today.toLocaleTimeString() + ".";
+    myObj.message = dateString;
+ 
+    res.json(myObj);
+});
 
 // Export the base-router
 export default router;
