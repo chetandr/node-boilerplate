7f5188f210d26010e654d18348809154d9074122
Poonam Rohani <poonam.rohani@seagate.com>
2020-07-13 21:18:51 +0530

    Added limit to handle large payload

--- src/Server.ts
+++ src/Server.ts
@@ -9,7 +9,7 @@ import cors from 'cors';
 const app = express();
 // Add middleware/settings/routes to express.
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 //app.use(verifyJwt)
